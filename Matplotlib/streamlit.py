c4205fd4faaa12b3cd2a054ac39f50d4c26a404f
Abhishek <abhishekganesh2002@gmail.com>
2024-09-10 19:30:39 +0530

    adding files

--- Matplotlib/streamlit.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.random.normal(170, 10, 250) #normal - normal distribution, mean(average) - 170, standard deviation - 10, no of random no - 250
-plt.hist(x)
-plt.show()
